3c48d586af4ed13e766aa9b5518ab6d953611c8c
Shahid-724 <shahid2472000@gmail.com>
2025-01-25 08:58:32 +0530

    Add files via upload

---
+++ 8_sort_arr.py
@@ -0,0 +1,16 @@
+# This code is used to sort the array 
+# Taking an array as input
+nums = list(map(int, input('Enter space separated numbers: ').split()))
+
+# Sorting the array using the inbuilt list method
+nums.sort()
+
+# Printing the result
+print(nums)
+
+# There are a total of five sorting algorithms that I need to learn
+# - Bubble sort
+# - Merge sort
+# - insertion sort
+# - quick sort
+# - selection sort
