50bd8a5e351bb11b90f91af238f2245063bf6695
Shahid-724 <shahid2472000@gmail.com>
2025-01-29 05:33:48 +0530

    Add files via upload

--- q3.py
+++ q3.py
@@ -1,16 +1,20 @@
-def findMaximumChange(temperatureChange):
-    result = float('-inf')
-    left = 0
-    total = sum(temperatureChange)
-    for i in range(len(temperatureChange)):
-        right = total - left
-        left += temperatureChange[i]
-        temp = max(left, right)
-        result = max(temp, result)
+def ramanAndHisPet(n, l):
+    result = 1 
+    if l[0] == 1:
+        result += 1
+    for i in range(1, len(l)):
+        print(result, i)
+        if l[i] == 0:
+            if l[i - 1] == 0:
+                return -1
+        else:
+            if l[i - 1] == 1:
+                result += 5
+            else:
+                print('last else')
+                result += 1
     return result
 
-temperatureChange = list(map(int, input().split()))
-print(findMaximumChange(temperatureChange))
-
-# Time Complexity: O(n)
-# Space Complexity: O(1)
+n = 3
+l = [1, 0, 1, 1]
+print(ramanAndHisPet(n, l))
