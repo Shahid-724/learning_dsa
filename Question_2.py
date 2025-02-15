ab7cafaf8f55c5d820f79a72d04c659bf8e449f5
Shahid-724 <shahid2472000@gmail.com>
2025-02-15 08:52:51 +0530

    Add files via upload

---
+++ Question_2.py
@@ -0,0 +1,10 @@
+# Printing mailing address
+# Taking input from the user
+name = input('Enter your name: ')
+door_no = input('Enter your door no.: ')
+street = input('Enter your street: ')
+city = input('Enter your city: ')
+pin_code = input('Enter your pincode: ')
+
+# Printing the address in required format
+print(f'{name}\n{door_no}, {street}\n{city}, {pin_code}')
