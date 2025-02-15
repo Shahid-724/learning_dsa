ca88daa2219f05f2180bfd7e5859f1558a07448d
Shahid-724 <shahid2472000@gmail.com>
2025-02-15 08:52:37 +0530

    Add files via upload

---
+++ Question_1.py
@@ -0,0 +1,8 @@
+# Displaying an equation
+# Taking input from the user
+a = int(input('Enter the value of a: '))
+b = int(input('Enter the value of b: '))
+c = int(input('Enter the value of c: '))
+
+# Displaying the equation
+print(f'{a}x + {b}y = {c}')
