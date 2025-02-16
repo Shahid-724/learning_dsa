73165f9a1d420be2a171d555bf27e8d46ddcddab
Shahid-724 <shahid2472000@gmail.com>
2025-02-16 10:38:47 +0530

    Add files via upload

---
+++ Question_6.py
@@ -0,0 +1,6 @@
+# Calculating the area of a circle
+# Taking input from the user
+radius = float(input('Enter the radius: '))
+
+# Calculating and printing the area
+print(f'The area of the circle is {3.1416 * radius * radius} sq units')
