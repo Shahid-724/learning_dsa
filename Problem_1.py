d7aeb2005dc95e79ddcd6996d28a6d842ca3ee7e
Shahid-724 <shahid2472000@gmail.com>
2025-02-21 08:04:21 +0530

    Add files via upload

---
+++ Problem_1.py
@@ -0,0 +1,11 @@
+# Checking the range of int in python
+number = 2147483647
+out_of_range = 214748364800
+
+print(number)
+print(out_of_range)
+
+# The out of range values of int will be automatically considered as float by the compiler
+# So no problem while using and printing the larger numbers
+
+# In python integers can be of infinite length
