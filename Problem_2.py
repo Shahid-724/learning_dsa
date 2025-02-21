193c436c3532e65528ff118ae27eba5ba13ceffe
Shahid-724 <shahid2472000@gmail.com>
2025-02-21 08:05:11 +0530

    Add files via upload

---
+++ Problem_2.py
@@ -0,0 +1,9 @@
+# Take input from the user and check if the number is greater than 100
+# Taking input from the user
+number = float(input('Enter a number: '))
+
+# The logic to check and print
+if number < 100:
+    print(f'{number} is smaller than 100')
+else: 
+    print(f'{number} contains more than two digits')
