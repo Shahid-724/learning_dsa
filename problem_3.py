9ee0456d678e428271c7242bc9bb7b406b5daadc
Shahid-724 <shahid2472000@gmail.com>
2025-02-23 09:45:59 +0530

    Add files via upload

---
+++ problem_3.py
@@ -0,0 +1,12 @@
+# This code takes a string as input
+# Copies the string into another variable and counts the no of chars
+
+# Taking input from the user
+str_1 = input('Enter a string: ')
+
+# Declaring a variable to copy the string
+str_2 = str_1
+
+# Printing the copied string and length
+print(str_2)
+print(len(str_2))
