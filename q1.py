5275fa445cf0a877750f5cd5c6835e9db16fb601
Shahid-724 <shahid2472000@gmail.com>
2025-02-24 08:57:42 +0530

    Add files via upload

--- q1.py
+++ q1.py
@@ -1,15 +1,13 @@
-def evaluation(S):
-    g, n, b = 0, 0, 0
-    for i in S:
-        if i == 'g':
-            g += 1
-        elif i == 'n':
-            n += 1
-        else:
-            b += 1
-    if g >= 1 and b == 0:
-        return 'Pass'
-    return 'Fail'
+def findNumber(numbers):
+    while len(numbers) > 2:
+        temp = []
+        for i in range(len(numbers) - 1):
+            temp.append(str(int(numbers[i]) + int(numbers[i + 1]))[-1])
+        numbers = temp.copy()
+    return str(numbers[0])[-1] + str(numbers[1])[-1]
 
-S = input()
-print(evaluation(S))
+numbers = list(map(int, input().split()))
+print(findNumber(numbers))
+
+# Time Complexity: O(n ^ 2)
+# Space Complexity: O(n)
