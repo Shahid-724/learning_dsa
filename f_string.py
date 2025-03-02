e0deacf2fd55b8d4450799d6287ccdd6c00b33b4
Shahid-724 <shahid2472000@gmail.com>
2025-03-02 11:24:40 +0530

    Add files via upload

---
+++ f_string.py
@@ -0,0 +1,9 @@
+# This code takes two strings as input
+# Prints the message
+
+# Taking input from the user
+name = input('Enter your name: ')
+sport = input('Which sport do you play: ')
+
+# Printing the output
+print(f'{name} is playing {sport    }')
