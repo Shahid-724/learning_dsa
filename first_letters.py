77a9eab9259bd4e5acfcb731503a1fdba9d5387d
Shahid-724 <shahid2472000@gmail.com>
2025-03-08 05:49:48 +0530

    Add files via upload

---
+++ first_letters.py
@@ -0,0 +1,10 @@
+# This code reads three words on seperate lines
+# Prints a string formed with first letter from each word
+
+# Taking input from the user
+word1 = input('> ')
+word2 = input('> ')
+word3 = input('> ')
+
+# Printing the output
+print(word1[0] + word2[0] + word3[0])
