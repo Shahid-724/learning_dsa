bb77435722c5116ebd7f4cf44d912a38dedeb184
Shahid-724 <shahid2472000@gmail.com>
2025-03-08 05:49:34 +0530

    Add files via upload

---
+++ first_last_char.py
@@ -0,0 +1,9 @@
+# This code reads a word as input
+# Prints the first and last characters in seperate lines
+
+# Taking input from the user
+word = input('> ')
+
+# Printing the first and last characters
+print(word[0])
+print(word[len(word) - 1])
