4cc3f1fc4e5b0f0ec1f7d64aad03b4ebb79417e5
Shahid-724 <shahid2472000@gmail.com>
2025-03-08 05:50:02 +0530

    Add files via upload

---
+++ reverse_order2.py
@@ -0,0 +1,11 @@
+# This code reads two words on seperate lines
+# Prints those words in reverse order seperated by ###
+
+# Taking input from the user
+word1 = input('> ')
+word2 = input('> ')
+
+# Printing the output
+print(word2)
+print('###')
+print(word1)
