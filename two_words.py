d983515e3e77683ae8f0b56b5cb2e342ab2aaac1
Shahid-724 <shahid2472000@gmail.com>
2025-03-09 11:23:35 +0530

    Add files via upload

---
+++ two_words.py
@@ -0,0 +1,10 @@
+# This code reads two words on seperate lines
+# The second word is at the beginning of the first word
+# Print the index at which first word ends in second word
+
+# Taking input from the user
+word1 = input('> ')
+word2 = input('> ')
+
+# Printing the output
+print(len(word2) - 1)
