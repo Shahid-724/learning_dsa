1b083249cc48b1e1caa5ed41a83034813bc859ce
Shahid-724 <shahid2472000@gmail.com>
2025-03-10 08:31:25 +0530

    Add files via upload

---
+++ half_string2.py
@@ -0,0 +1,8 @@
+# This code takes a string as input
+# Prints the second part of the string
+
+# Taking input from the user
+word = input('> ')
+
+# Printing the second half
+print(word[len(word) // 2:])
