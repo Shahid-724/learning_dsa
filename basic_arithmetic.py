48bb4a6f59c9487289c6add1baa5d607abe72b49
Shahid-724 <shahid2472000@gmail.com>
2025-03-10 08:31:05 +0530

    Add files via upload

---
+++ basic_arithmetic.py
@@ -0,0 +1,11 @@
+# This code takes two integers as input
+# Prints the sum, difference and product of those two numbers
+
+# Taking input from the user
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+# Printing the output
+print(f'The sum is {num1 + num2}')
+print(f'The difference is {num1 - num2}')
+print(f'The product is {num1 * num2}')
