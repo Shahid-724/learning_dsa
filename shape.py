8bed638e87a988cb9b194e96eedd46ba54cd051d
Shahid-724 <shahid2472000@gmail.com>
2025-03-11 08:40:39 +0530

    Add files via upload

---
+++ shape.py
@@ -0,0 +1,9 @@
+# This code takes a number as input
+# Prints three lines with n +s on each line
+
+# Taking input from the user
+n = int(input('Enter the no. of +s on each line: '))
+
+# Printing the output
+for i in range(0, 3):
+    print('+ ' * n)
