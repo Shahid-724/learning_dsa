452e42a92ee8eb00554f06838f4c9c47039d45bc
Shahid-724 <shahid2472000@gmail.com>
2025-03-11 08:40:15 +0530

    Add files via upload

---
+++ square_ap.py
@@ -0,0 +1,9 @@
+# This code takes the side of a square as input
+# Prints the area and perimeter of the square
+
+# Taking input from the user
+side = int(input('Enter the side of the square: '))
+
+# Printing the area and perimeter
+print(f'The area of the square is {side ** 2} sq.units')
+print(f'The perimeter of the square is {side * 4} units')
