72d502d8b7461420262e1985c0b51de17d879844
Shahid-724 <shahid2472000@gmail.com>
2025-03-11 08:41:20 +0530

    Add files via upload

---
+++ percentage2.py
@@ -0,0 +1,8 @@
+# This code takes an integer as input
+# Prints seventy percent of that number
+
+# Taking input from the user
+num = int(input('Enter a number: '))
+
+# Printing 70 percent of that number
+print(num * 0.7)
