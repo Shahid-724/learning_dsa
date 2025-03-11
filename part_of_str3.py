6aa81163d2ccdb01a0e31b245a72e2a9be429e3c
Shahid-724 <shahid2472000@gmail.com>
2025-03-11 08:41:46 +0530

    Add files via upload

---
+++ part_of_str3.py
@@ -0,0 +1,8 @@
+# This code takes a string as input
+# Prints the string without the first two and last two letters
+
+# Taking input from the user
+word = input('Enter the word: ')
+
+# Printing the output
+print(word[2:-2])
