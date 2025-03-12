a8146cd06a0f9cc7e9d98f4f9ace5a072c4345ea
Shahid-724 <shahid2472000@gmail.com>
2025-03-12 09:03:02 +0530

    Add files via upload

---
+++ full_name.py
@@ -0,0 +1,10 @@
+# This code reads two words on seperate lines
+# Joins those words with a space in between
+# Prints the joined word
+
+# Taking input from the user
+word1 = input('> ')
+word2 = input('> ')
+
+# Joining and printing
+print(word1 + ' ' + word2)
