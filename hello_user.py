adc92e5eb445285efb3614578edb454d457ffd54
Shahid-724 <shahid2472000@gmail.com>
2025-03-12 09:04:36 +0530

    Add files via upload

---
+++ hello_user.py
@@ -0,0 +1,8 @@
+# This code takes a single word as input
+# Prints hello followed by that word
+
+# Taking input from the user
+word = input('> ')
+
+# Printing in required format
+print(f'Hello {word}')
