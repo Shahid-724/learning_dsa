8e71162140302120d0bbd7453a067f134adda5b4
Shahid-724 <shahid2472000@gmail.com>
2025-03-14 05:29:23 +0530

    Add files via upload

---
+++ print_input4.py
@@ -0,0 +1,8 @@
+# This code takes a single line input
+# Prints the input after the message 'Given input: '
+
+# Taking input from the user
+word = input('> ')
+
+# Printing the output in required format
+print(f'Given input: {word}')
