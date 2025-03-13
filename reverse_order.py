1a0100d019db20a588de0047ee6dd68013294c66
Shahid-724 <shahid2472000@gmail.com>
2025-03-14 05:29:09 +0530

    Add files via upload

---
+++ reverse_order.py
@@ -0,0 +1,10 @@
+# This code reads two lines of input
+# Prints these two inputs in reverse order
+
+# Taking input from the user
+word1 = input('> ')
+word2 = input('> ')
+
+# Printing in reverse order
+print(word2)
+print(word1)
