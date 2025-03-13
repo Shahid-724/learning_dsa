0bb7e943ee89630fe404f0453da2e9083047a8e2
Shahid-724 <shahid2472000@gmail.com>
2025-03-14 05:28:55 +0530

    Add files via upload

---
+++ second_line.py
@@ -0,0 +1,9 @@
+# This code reads two inputs on seperate lines
+# Prints only the second input
+
+# Taking input from the user
+word1 = input('> ')
+word2 = input('> ')
+
+# Printing the second input
+print(word2)
