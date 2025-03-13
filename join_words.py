7023c05a21e0a0c7dc375cda62b63b69326e50fc
Shahid-724 <shahid2472000@gmail.com>
2025-03-13 08:17:17 +0530

    Add files via upload

---
+++ join_words.py
@@ -0,0 +1,9 @@
+# This code reads two words in two seperate lines
+# Prints the join of those two words
+
+# Taking input from the user
+word1 = input('> ')
+word2 = input('> ')
+
+# Printing the joined word
+print(word1 + word2)
