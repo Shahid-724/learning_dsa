18b34e30e91389ca44df9471862a9de1ad831046
Shahid-724 <shahid2472000@gmail.com>
2025-03-14 05:29:36 +0530

    Add files via upload

---
+++ print_input3.py
@@ -0,0 +1,10 @@
+# This code reads two inputs on two seperate lines
+# Prints those two outputs on seperate lines
+
+# Taking input from the user
+word1 = input('> ')
+word2 = input('> ')
+
+# Printing the output
+print(word1)
+print(word2)
