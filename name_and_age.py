dfb2306435d3588218a1404538c502a1ac692c52
Shahid-724 <shahid2472000@gmail.com>
2025-03-13 08:18:04 +0530

    Add files via upload

---
+++ name_and_age.py
@@ -0,0 +1,9 @@
+# This code reads a string and an integer
+# Prints them in a specified format
+
+# Taking input from the user
+name = input('Enter your name: ')
+age = int(input('Enter your age: '))
+
+# Printing them in the specified format
+print(f'{name} is {age} years old')
