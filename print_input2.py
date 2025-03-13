d39781c2fe7bbad5575d55a6f48cc747e0d88b32
Shahid-724 <shahid2472000@gmail.com>
2025-03-13 08:18:38 +0530

    Add files via upload

---
+++ print_input2.py
@@ -0,0 +1,10 @@
+# This code reads a single line input
+# Prints that input
+# Prints three hashes on the next line
+
+# Taking input from the user
+user_input = input('> ')
+
+# Printing the input and hashes
+print(user_input)
+print('###')
