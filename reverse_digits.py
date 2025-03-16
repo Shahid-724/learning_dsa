4fce7bac60498b0ecbc7853d185d3e746807812a
Shahid-724 <shahid2472000@gmail.com>
2025-03-16 12:11:06 +0530

    Add files via upload

---
+++ reverse_digits.py
@@ -0,0 +1,9 @@
+# This code reads a number in the form of a string
+# The number consists of only two digits
+# Print the digits in reverse order
+
+# Taking input from the user
+num = input('> ')
+
+# Printing the digits in reverse order
+print(num[1] + num[0])
