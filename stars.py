3cfc9521e10b6271762285b54a96549c2c1ffdc7
Shahid-724 <shahid2472000@gmail.com>
2025-03-17 08:29:49 +0530

    Add files via upload

---
+++ stars.py
@@ -0,0 +1,8 @@
+# This code reads a word
+# Prints the word with three stars on each side
+
+# Taking input from the user
+word = input('> ')
+
+# Printing in required format
+print(f'* * * {word} * * *')
