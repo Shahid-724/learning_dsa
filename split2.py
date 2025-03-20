a4dce0fa0c56cea8654634350701eb7a34e0a004
Shahid-724 <shahid2472000@gmail.com>
2025-03-20 08:35:05 +0530

    Add files via upload

---
+++ split2.py
@@ -0,0 +1,12 @@
+# This code takes a sentence as input
+# Prints each word of the sentence on a different line
+
+# Taking input from the user
+sentence = input('Enter a sentence: ')
+
+# Splitting the sentence into words
+words = sentence.split()
+
+# Printing the words on different lines
+for word in words:
+    print(word)
