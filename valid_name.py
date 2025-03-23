780d743fa75d182e0fb9b1dd4ac0d636d7e68125
Shahid-724 <shahid2472000@gmail.com>
2025-03-23 10:40:57 +0530

    Add files via upload

---
+++ valid_name.py
@@ -0,0 +1,18 @@
+# This code takes a string as input
+# Checks whether it is a valid variable name
+
+# Taking input from the user
+word = input('Enter the name of your variable: ')
+
+# Declaring a boolean for checking
+is_valid = True
+
+# Checking if the name is valid
+for char in word:
+    if not char.isalnum() or word[0].isdigit():
+        is_valid = False
+    if char == '_':
+        is_valid = True
+
+# Printing the output
+print(is_valid)
