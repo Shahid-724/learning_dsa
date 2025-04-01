2c3685e2266bc98f4aef89cda35d699b9b1ceaa2
Shahid-724 <shahid2472000@gmail.com>
2025-04-01 07:54:16 +0530

    Add files via upload

---
+++ unicode.py
@@ -0,0 +1,9 @@
+# This code takes a string as input
+# Prints the unicode values of all chars
+
+# Taking input from the user
+word = input('Enter a string: ')
+
+# Printing the output
+for char in word:
+    print(ord(char))
