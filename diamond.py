d9fd68ebf010de06337515aefb0f72945e279343
Shahid-724 <shahid2472000@gmail.com>
2025-04-04 08:27:02 +0530

    Add files via upload

--- diamond.py
+++ diamond.py
@@ -1,14 +1,27 @@
 # This code takes an integer as input
-# Prints a diamond crystal
+# Prints a hollow diamond using continuous alphabets
 
 # Taking input from the user
 n = int(input('Enter the size of the diamond: '))
 
+# Declaring a variable for alphabets
+alphabet = 65
+
 # Printing the diamond
-for i in range(2 * n):
+for i in range(2 * n - 1):
     line = ''
-    if i < n:
-        line += ' ' * (n - i - 1) + '/' + ' ' * 2 * (i) + '\\'
+    if i == 0 or i == 2 * n - 2:
+        line += ' ' * (n - 1) + 'A'
+    elif i < n:
+        line += ' ' * (n - i - 1)
+        for j in range(2):
+            alphabet += 1
+            line += f'{chr(alphabet)}' + ' ' * (2 * i - 1)
     else:
-        line += ' ' * (i - n) + '\\' + ' ' * 2 * (2 * n - i - 1) + '/'
+        line += ' ' * (i - n + 1)
+        alphabet -= 3
+        for j in range(2):
+            line += f'{chr(alphabet)} ' + '  ' * (2 * n - i - 3)
+            alphabet += 1
+        alphabet -= 1
     print(line)
