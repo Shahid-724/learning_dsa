b9e0efe6fc77814164fd6c1024afe5c4528f98db
Shahid-724 <shahid2472000@gmail.com>
2025-04-06 11:50:23 +0530

    Add files via upload

--- average.py
+++ average.py
@@ -1,17 +1,15 @@
-# This code takes two integers as input
-# Prints the sum and average for all no in that range
+# This code takes n integers as input
+# Prints the average of these numbers
 
 # Taking input from the user
-m = int(input('Enter the starting no: '))
-n = int(input('Enter the ending no: '))
+numbers = input('Enter space seperated numbers: ').split()
 
 # Declaring a variable for sum
 sum = 0
 
 # Calculating the sum
-for i in range(m, n + 1):
-    sum += i
+for i in numbers:
+    sum += int(i)
 
-# Printing the output
-print(sum)
-print(sum / (n - m + 1))
+# Printing the average
+print(f'{(sum / len(numbers)):.2f}')
