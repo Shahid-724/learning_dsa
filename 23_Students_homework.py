dcfe6139379cba4220ebd8333d52b039a3dd5f17
Shahid-724 <shahid2472000@gmail.com>
2025-04-25 09:03:20 +0530

    Add files via upload

---
+++ 23_Students_homework.py
@@ -0,0 +1,13 @@
+class Solution:
+    def busyStudent(self, startTime: list[int], endTime: list[int], queryTime: int) -> int:
+        
+        res = 0
+
+        for i in range(len(startTime)):
+            if startTime[i] <= queryTime <= endTime[i]:
+                res += 1
+
+        return res
+    
+# Time - O(N)
+# Space - O(1)
