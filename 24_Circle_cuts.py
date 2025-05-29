14f851bbf2f3db874147415a6c944c07002b6800
Shahid-724 <shahid2472000@gmail.com>
2025-05-29 08:46:39 +0530

    Add files via upload

---
+++ 24_Circle_cuts.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfCuts(self, n: int) -> int:
+
+        if n == 1:
+            return 0
+        
+        if n % 2 == 0:
+            return n // 2
+        return n
+    
+# Time - O(1)
+# Space - O(1)
