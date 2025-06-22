e47bfcf520e5ba3d1df76f2b46f1e415b3f8d11a
Shahid-724 <shahid2472000@gmail.com>
2025-06-22 10:11:12 +0530

    Add files via upload

---
+++ 65_Sum_multiples.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        
+        res = 0
+
+        for i in range(1, n + 1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                res += i
+
+        return res
+    
+# Time - O(N)
+# Space - O(1)
