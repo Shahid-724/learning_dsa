47bed15caa200d98273626bee038da609fe585f7
Shahid-724 <shahid2472000@gmail.com>
2025-09-14 10:33:54 +0530

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,17 @@
+ # The code
+line01 = '********************' # Header/Footer
+line02 = '*                  *' # re-use
+line03 = '*     WELCOME!     *'
+
+# Starts with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+# Notes
+# - Comments are notes that we leave for other programmers or ourselves
+# - In python indentations have special meanings
+# - Python does not ignore white space at the beginning of a line
