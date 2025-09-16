01f682cb50c2d2755a06496a8c90e9c142e1b03d
Shahid-724 <shahid2472000@gmail.com>
2025-09-16 09:02:01 +0530

    Add files via upload

--- 6_hcf.py
+++ 6_hcf.py
@@ -1,10 +1,23 @@
-# This code is used to find the hcf of two numbers
+# This code is used to find the hcf of two numbers using recursion
+# Taking input from the user
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
 
-def hcf(a, b):
-    if b == 0:
-        return a
-    return hcf(b, a % b)
+# Defining an iterative function to get hcf
+def hcf(n1, n2):
+    result = 1
+    for i in range(2, min(n1, n2) + 1):
+        if n1 % i == 0 and n2 % i == 0:
+            result = i
+    return result
 
-a = int(input('Enter first num: '))
-b = int(input('Enter second num: '))
-print(hcf(a, b))
+# Defining a recursive function to get hcf
+def hcf_rec(n1, n2):
+    n1, n2 = max(n1, n2), min(n1, n2)
+    if n1 % n2 == 0:
+        return n2
+    return hcf_rec(n2, n1 % n2)
+
+# Calling the functions and printing the output
+print(hcf(num1, num2))
+print(hcf(num1, num2))
